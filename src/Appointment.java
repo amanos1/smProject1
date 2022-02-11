06135e04ab83aba7568a380eda351ab04cca2e8c
harshPatel1818 <91505652+harshPatel1818@users.noreply.github.com>
2022-02-11 10:28:47 -0500

    Update Appointment.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -2,51 +2,53 @@ package src;
 
 public class Appointment 
 {
-    private Patient patient;
-    private Timeslot slot;
-    private Location location;
-    
-    public Appointment(Patient p, Timeslot t, Location l)
-    {
-        patient = p;
-        slot = t;
-        location = l;
-    } 
-    
-    public Patient getPatient() 
-    {
-        Patient p = new Patient(patient);
-        return p;
-    }
-    
-    public Timeslot getTimeslot() 
-    {
-        Timeslot ts = new Timeslot(slot);
-        return ts;
-    }
-    
-    public Location getLocation() 
-    {
-        return location;
-    }
-    
-    public int compareTo(Appointment a) 
-    {
-        return 0;
-    }
-    
-    public boolean equals(Appointment a) 
-    {
-        return (patient.equals(a.getPatient()) && slot.equals(a.getTimeslot())&&
-                location.equals(a.getLocation()));
-    }
-    
-    public String toString() 
-    {
-        return (patient.toString()+
-                ", Appointment detail: "+slot.toString()+
-                ", "+location.toString());
-    }
-    
+  private Patient patient;
+	private Timeslot slot;
+	private Location location;
+	
+	public Appointment(Patient p, Timeslot t, Location l)
+	{
+		this.patient = p;
+		this.slot = t;
+		this.location = l;
+		
+	} 
+	
+	public Patient getPatient() 
+	{
+		Patient p = new Patient(this.patient);
+		return p;
+	}
+	
+	public Timeslot getTimeslot() 
+	{
+		Timeslot ts = new Timeslot(this.slot);
+		return ts;
+	}
+	
+	public Location getLocation() 
+	{
+		return this.location;
+	}
+	//NEED TO FINISH THIS METHOD
+	public int compareTo(Appointment a) 
+	{
+		return 0;
+	}
+	
+	public boolean equals(Appointment a) 
+	{
+		return (this.patient.equals(a.getPatient()) && this.slot.equals(a.getTimeslot())&&
+				this.location.equals(a.getLocation()));
+	}
+	
+	public String toString() 
+	{
+		return (patient.toString()+
+				", Appointment detail: "+this.slot.toString()+
+				", "+this.location.toString());
+	}
+	
+	
     
 }
