531a0db94ba97853c48cece9e3fba2b601ffcd4d
thing-maker <ajb445@scarletmail.rutgers.edu>
2022-02-12 16:57:58 -0500

    Date fully commented

--- src/Date.java
+++ src/Date.java
@@ -1,20 +1,34 @@
 package src;
 import java.util.Calendar;
 
-public class Date implements Comparable<Date>{
-
-   private int year;
+/**
+ * Contains information about a single date.
+ * This includes the month, day of month, and year.
+ * Can determine if the date is a valid calendar date.
+ * @author Aaron Browne, Harshkumar Patel
+ */
+public class Date implements Comparable<Date>
+{
+	private int year;
 	private int month;
 	private int day;
-	Calendar calendar = Calendar.getInstance();
-	
+
+	/**
+	 * When given no arguments, the constructor creates an instance of date with the current date,
+	 * as provided by Java's Calendar library.
+	 */
 	public Date() 
 	{
+		Calendar calendar = Calendar.getInstance();
 		this.month  = calendar.get(Calendar.MONTH);
 		this.year = calendar.get(Calendar.YEAR);
 		this.day = calendar.get(Calendar.DATE);
 	}
 
+	/**
+	 * When given a string, this constructor parses the string and creates an instance of date with it.
+	 * @param date The date string in mm/dd/yyyy format.
+	 */
     public Date(String date)
     {
     	String[] dateElements = date.split("/");
@@ -23,12 +37,23 @@ public class Date implements Comparable<Date>{
         year = Integer.parseInt(dateElements[2]);
     }
 
-	public Date(Date d) {
+    /**
+     * Creates a clone of the date provided.
+     * @param d The date to clone.
+     */
+	public Date(Date d)
+	{
 		this.month = d.getMonth();
 		this.day = d.getDay();
 		this.year = d.getYear();
 	}
 
+	/**
+	 * Creates an instance of Date with the month, day and year provided.
+	 * @param m The month.
+	 * @param d The day of the month.
+	 * @param y The year.
+	 */
 	public Date(int m, int d, int y) 
 	{
 		this.month = m;
@@ -36,46 +61,76 @@ public class Date implements Comparable<Date>{
 		this.year = y;
 	}
 
-	public int getDay() 
+	/**
+	 * Returns the day of the month
+	 * @return The day of the month
+	 */
+	public int getDay()
 	{
 		int d = this.day;
 		return d;
 	}
 
+	/**
+	 * Returns the month.
+	 * @return The month.
+	 */
 	public int getMonth() 
 	{
 		int m = this.month;
 		return m;
 	}
 
+	/**
+	 * Returns the year.
+	 * @return The year.
+	 */
 	public int getYear() 
 	{
 		int y = this.year;
 		return y;
 	}
 
+	/**
+	 * Changes the day of the month.
+	 * @param d The new day of the month.
+	 */
 	public void setDay(int d) 
 	{
 		this.day = d;
 	}
 
+	/**
+	 *  Changes the month.
+	 * @param m The new month.
+	 */
 	public void setMonth(int m) 
 	{
 	    this.month = m;
 	}
 
+	/**
+	 * Changes the year.
+	 * @param y The new year.
+	 */
 	public void setYear(int y) 
 	{
 		this.year = y;
 	}
 
+	/**
+	 * Determines whether or not the date is a valid calendar date.
+	 * Does not allow years before 1900 or after the current one.
+	 * Takes into account the amount of days of different months and leap years.
+	 * @return true if the date is valid, false if not.
+	 */
 	public boolean isValid()
 	{
 		final int TOTAL_MONTH = 12;
 		final int MIN_MONTH = 1;
 		final int TOTAL_FEB_DAYS = 28;
 		final int TOTAL_FEB_LEAP_DAYS = 29;
-		final int TOTAL_AJSN_DAYS = 30;
+		final int TOTAL_AJSN_DAYS = 30; //AJSN = April, June, September, and November
 		final int TOTAL_MONTH_DAYS = 31;
 		boolean leapYear = isLeapYear(year);
 		Calendar today = Calendar.getInstance();
@@ -105,6 +160,11 @@ public class Date implements Comparable<Date>{
 		return true;
 	}
 
+	/**
+	 * Determines whether or not the given year is a leap year.
+	 * @param y The year to analyze.
+	 * @return true if the year is a leap year, false if not.
+	 */
 	private boolean isLeapYear(int y) 
 	{
 		final int QUADRENNIAL = 4;
@@ -131,11 +191,22 @@ public class Date implements Comparable<Date>{
 	    }
 	}
 
+	/**
+	 * Determine whether two dates are equal.
+	 * @param date The date to compare against.
+	 * @return true if the dates are equal, false if not.
+	 */
 	public boolean equals(Date date)
 	{
 		return compareTo(date) == 0;
 	}
 
+	/**
+	 * Compares two dates.
+	 * @param date The date to compare the current one to.
+	 * @return 1 if the current date occurs after the given one, -1 if the current date occurs before, and 0 if the the dates are the same.
+	 */
+	@Override
 	public int compareTo(Date date)
 	{
 		if(year > date.getYear()) return 1;
@@ -147,11 +218,20 @@ public class Date implements Comparable<Date>{
 		return 0;
 	}
 
+	/**
+	 * Converts the date into a string
+	 * @return A string representation of the date in mm/dd/yyyy format.
+	 */
 	public String toString()
 	{
 		return month + "/" + day + "/" + year;
 	}
 
+	/**
+	 * Tries a series of tests to make sure if isValid() function works.
+	 * All the tests should return false except for the last one.
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		Date d;
